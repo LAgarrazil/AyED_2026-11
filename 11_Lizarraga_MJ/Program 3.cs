bdc24bf62d6f577e81b5f91f325acefd4666cc70
LAgarrazil <leonardolizarraga245@gmail.com>
2026-03-26 16:56:55 -0300

    Add files via upload

---
+++ 11_Lizarraga_MJ/Program 3.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP_1_Lizarraga_4_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.Write("año de nacimiento: ");
+            int año = Convert.ToInt32(Console.ReadLine());
+
+            int edad = 2026 - año;
+           
+
+
+            Console.Write("Si naciste en el año " + año + " tu edad al comenzar el año es de " + edad + " años.");
+            
+
+        }
+    }
+}
