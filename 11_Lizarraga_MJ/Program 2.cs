465fbd226dec889b195e2a79eaa2495468cfd42e
LAgarrazil <leonardolizarraga245@gmail.com>
2026-03-26 16:32:33 -0300

    Add files via upload

---
+++ 11_Lizarraga_MJ/Program 2.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP_1_Lizarraga_4_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hola ususario");
+            string nombre;
+            
+            Console.Write("cual es tu nombre: ");
+            nombre = Console.ReadLine();
+            string juego;
+
+            Console.Write("juego favorito: ");
+            juego = Console.ReadLine();
+            string consola;
+
+            Console.Write("consola en la que jugas: ");
+            consola = Console.ReadLine();
+
+
+            Console.WriteLine("Hola " + nombre + ". Tu juego favorito es " + juego + " y se juega en " + consola);
+            
+
+        }
+    }
+}
