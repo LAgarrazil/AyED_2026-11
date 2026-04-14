5e182aa4682b2dc003e0366e5862ef679e61ed27
LAgarrazil <leonardolizarraga245@gmail.com>
2026-04-14 16:56:44 -0300

    Add files via upload

---
+++ Ejercicios Condicionales - Primera parte/Aprobación de Examen.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double nota;
+            Console.Write("tu nota: ");
+            nota = Convert.ToInt32(Console.ReadLine());
+
+            if (nota >= 7 & nota <= 10)
+            {
+                Console.WriteLine("promocionado");
+            }
+            else if (nota >= 4 & nota <= 10)
+            {
+                Console.WriteLine("a finales");
+            }
+            else
+            {
+                Console.WriteLine("recuperatorio");
+            }
+        }
+    }
+}
