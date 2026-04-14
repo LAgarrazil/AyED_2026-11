2d66d3609a4cee8365846bbaf47a561c8bb015a0
LAgarrazil <leonardolizarraga245@gmail.com>
2026-04-14 14:40:01 -0300

    Add files via upload

---
+++ Ejercitacion de IF ELSE/Clasificador de Aspirantes a la Academia Jedi.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int midclorianos;
+            Console.Write("peso en la nave(kg): ");
+            midclorianos = Convert.ToInt32(Console.ReadLine());
+            if (midclorianos >= 5000 & midclorianos <= 15000)
+
+            {
+                Console.WriteLine("Aceptable para entrenamiento Padawan");
+            }
+            else
+            {
+                Console.WriteLine("No posee sensibilidad a la Fuerza");
+            }
+
+
+
+        }
+    }
+}
