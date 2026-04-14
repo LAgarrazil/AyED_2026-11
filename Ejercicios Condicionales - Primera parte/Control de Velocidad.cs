6588c3ac2c1f847823e578438c5b99aa2ba112dc
LAgarrazil <leonardolizarraga245@gmail.com>
2026-04-14 16:34:06 -0300

    Add files via upload

---
+++ Ejercicios Condicionales - Primera parte/Control de Velocidad.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int velocidad;
+            Console.Write("velocidad: ");
+            velocidad = Convert.ToInt32(Console.ReadLine());
+            if (velocidad < 120)
+            {
+                Console.WriteLine("Multa en camino: Exceso de velocidad");
+            }
+            else
+            {
+                Console.WriteLine("Conducción segura".);
+            }
+        }
+    }
+}
