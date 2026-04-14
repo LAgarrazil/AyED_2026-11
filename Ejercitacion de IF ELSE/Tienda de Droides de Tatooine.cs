da8835bfa8797771d7bbf25b9ccfaa9a7161ffbe
LAgarrazil <leonardolizarraga245@gmail.com>
2026-04-14 15:51:42 -0300

    Add files via upload

---
+++ Ejercitacion de IF ELSE/Tienda de Droides de Tatooine.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int droide;
+            int precio;
+            int descuento;
+            int total;
+            Console.Write("cantidad de droides: ");
+            droide = Convert.ToInt32(Console.ReadLine());
+            precio = droide * 2000;
+            if (droide >= 3)
+            {
+                descuento = (precio * 20) / 100;
+                total = precio - descuento;
+                Console.WriteLine("precio: " + precio );
+                Console.WriteLine("descuento: " + descuento);
+                Console.WriteLine("total: " + total);
+            }
+            else
+            {
+                Console.WriteLine("precio: " + precio);
+            }
+
+        }
+    }
+}
