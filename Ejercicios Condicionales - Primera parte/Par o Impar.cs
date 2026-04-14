e6a618a24cc7f5aa22aac21208afe4657420831a
LAgarrazil <leonardolizarraga245@gmail.com>
2026-04-14 16:48:47 -0300

    Add files via upload

---
+++ Ejercicios Condicionales - Primera parte/Par o Impar.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero;
+            Console.Write("numero entero: ");
+            numero = Convert.ToInt32(Console.ReadLine());
+
+            if (numero % 2 == 0)
+            {
+                Console.WriteLine(numero + " es par");
+            }
+            else
+            {
+                Console.WriteLine(numero + " es impar");
+            }
+        }
+    }
+}
