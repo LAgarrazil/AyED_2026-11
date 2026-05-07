bb64937422b0a3dda28638d89b27d997fadd1a2c
LAgarrazil <leonardolizarraga245@gmail.com>
2026-05-07 15:19:53 -0300

    Add files via upload

---
+++ Switch - Contador - Acumulador - FOR - Basico/Menú de Restaurante.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string opcion;
+            Console.WriteLine("S.Sandwich");
+            Console.WriteLine("P.Pizza");
+            Console.WriteLine("H.Hamburguesa");
+            Console.Write("Opcion deseada: ");
+            opcion = Console.ReadLine();
+
+
+            switch (opcion)
+            {
+                case "S":
+                    Console.WriteLine("sandwich");
+                    break;
+
+
+                case "P":
+                    Console.WriteLine("pizza");
+                    break;
+
+                case "H":
+                    Console.WriteLine("hamburguesa");
+                    break;
+
+                default:
+                    Console.WriteLine("producto no encontrado");
+                    break;
+            }
+        }
+    }
+}
---
+++ Switch - Contador - Acumulador - FOR - Basico/Registro de Temperaturas.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int grados = 0;
+            int contador = 0;
+
+            for (int i = 1; i <= 5; i++)
+            {
+                Console.WriteLine("grados dia " + i);
+                grados = Convert.ToInt32(Console.ReadLine());
+                if (grados < 0)
+                {
+                    contador++;
+                }
+            }
+            Console.WriteLine("dias de frio extremo: " + contador);
+
+
+
+        }
+    }
+}
