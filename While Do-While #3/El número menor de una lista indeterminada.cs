802a09e843c0867d3a88737f57d7778c303472f1
LAgarrazil <leonardolizarraga245@gmail.com>
2026-06-11 14:38:59 -0300

    Add files via upload

---
+++ While Do-While #3/El número menor de una lista indeterminada.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero;
+            int minimo = 0;
+            bool primerNumero = true;
+
+            do
+            {
+                Console.Write("Ingrese un número (negativo para finalizar): ");
+                numero = int.Parse(Console.ReadLine());
+
+                if (numero >= 0)
+                {
+                    if (primerNumero)
+                    {
+                        minimo = numero;
+                        primerNumero = false;
+                    }
+                    else if (numero < minimo)
+                    {
+                        minimo = numero;
+                    }
+                }
+
+            } while (numero >= 0);
+
+            if (!primerNumero)
+            {
+                Console.WriteLine("número menor: " + minimo);
+            }
+            else
+            {
+                Console.WriteLine("No se ingresaron números positivos.");
+            }
+        }
+    }
+}
---
+++ While Do-While #3/Simulador de dados con apuestas.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int creditos = 100;
+            int apuesta;
+
+            do
+            {
+                Console.WriteLine("Créditos : " + creditos);
+                Console.Write("apuesta (0 para retirarse): ");
+                apuesta = int.Parse(Console.ReadLine());
+
+                if (apuesta > 0 && apuesta <= creditos)
+                {
+                    Console.Write("dado 1 (1-6): ");
+                    int dado1 = int.Parse(Console.ReadLine());
+
+                    Console.Write("dado 2 (1-6): ");
+                    int dado2 = int.Parse(Console.ReadLine());
+
+                    int suma = dado1 + dado2;
+
+                    if (suma == 7 || suma == 11)
+                    {
+                        creditos += apuesta;
+                        Console.WriteLine("Ganaste");
+                    }
+                    else
+                    {
+                        creditos -= apuesta;
+                        Console.WriteLine("Perdiste.");
+                    }
+                }
+                else if (apuesta != 0)
+                {
+                    Console.WriteLine("Apuesta inválida.");
+                }
+
+            } while (creditos > 0 && apuesta != 0);
+
+            Console.WriteLine("Saldo final: " + creditos + " créditos");
+        }
+    }
+}
