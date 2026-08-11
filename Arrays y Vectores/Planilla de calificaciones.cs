28b533ae0cdbac64276b07ee44c235eda558d2a8
LAgarrazil <leonardolizarraga245@gmail.com>
2026-08-11 16:26:21 -0300

    Add files via upload

---
+++ Arrays y Vectores/Planilla de calificaciones.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] nota = new double[10];
+            double total = 0;
+            int alumno = 0;
+            int aprobados = 0;
+            int desaprobados = 0;
+            for (int i = 0; i < nota.Length; i++)
+            {
+                alumno++;
+                Console.WriteLine("nota de alumno n°: " + alumno);
+                nota[i] = Convert.ToDouble(Console.ReadLine());
+                total += nota[i];
+                if (nota[i] >= 6)
+                {
+                    aprobados++;
+                }
+                else 
+                {
+                    desaprobados++;
+                }
+            }
+            double promedio = total / nota.Length;
+            Console.WriteLine("alumnos aprobados: " + aprobados);
+            Console.WriteLine("alumnos desaprobados: " + desaprobados);
+            Console.WriteLine("promedio de notas: " + promedio);
+        }
+    }
+}
+
---
+++ Arrays y Vectores/Registro semanal de temperaturas.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] temperatura = new double[7];
+            double total = 0;
+            int dia = 0;
+            for (int i = 0; i < temperatura.Length; i++)
+            {
+                dia++;
+                Console.WriteLine("temperatura dia: " + dia);
+                temperatura[i] = Convert.ToDouble(Console.ReadLine());
+                total += temperatura[i];
+            }
+            double mayor = temperatura[0];
+            double menor = temperatura[0];
+            for (int i = 0; i < temperatura.Length; i++)
+            {
+               
+                if (temperatura[i] > mayor)
+                {
+                    
+                    mayor = temperatura[i];
+                }
+                else if (temperatura[i] < menor)
+                {
+                    menor = temperatura[i];
+                }
+            }
+            double promedio = total / temperatura.Length;
+            Console.WriteLine("temeratura mayor: " + mayor);
+            Console.WriteLine("temeratura menor: " + menor);
+            Console.WriteLine("temeratura promedio: " + promedio);
+        }
+    }
+}
