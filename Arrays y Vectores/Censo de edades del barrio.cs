d5877f5ae8b9f85f11bcfe1f52af94008ff15ace
LAgarrazil <leonardolizarraga245@gmail.com>
2026-08-18 15:20:44 -0300

    Add files via upload

---
+++ Arrays y Vectores/Censo de edades del barrio.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] edad = new double[10];
+            int vecino = 0;
+            int menores = 0;
+            int mayores = 0;
+            int promedio = 0;
+            for (int i = 0; i < edad.Length; i++)
+            {
+                vecino++;
+                Console.WriteLine("edad de vecino n°: " + vecino);
+                edad[i] = Convert.ToDouble(Console.ReadLine());
+                if (edad[i] <= 17)
+                {
+                    menores++;
+                }
+                else if (edad[i] >= 65)
+                {
+                    mayores++;
+                }
+                else
+                {
+                    promedio++;
+                }
+            }
+            Console.WriteLine("vecinos menores: " + menores);
+            Console.WriteLine("vecinos mayores: " + mayores);
+            Console.WriteLine("vecinos de edad promedio: " + promedio);
+        }
+    }
+}
