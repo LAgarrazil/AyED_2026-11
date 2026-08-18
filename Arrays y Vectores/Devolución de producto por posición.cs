347382c6ca560650e74c685e9aeb07432084915e
LAgarrazil <leonardolizarraga245@gmail.com>
2026-08-18 14:46:58 -0300

    Add files via upload

---
+++ Arrays y Vectores/Devolución de producto por posición.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] precio = new double[10];
+            int devolucion = 0;
+            int producto = 0;
+            double total = 0;
+            for (int i = 0; i < precio.Length; i++)
+            {
+                producto++;
+                Console.WriteLine("precio producto n°: " + producto);
+                precio[i] = Convert.ToDouble(Console.ReadLine());
+                total += precio[i];
+            }
+            Console.WriteLine("total: " + total);
+            Console.WriteLine("devolucion de producto n°: ");
+            devolucion = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < precio.Length; i++)
+            {
+                if (precio[i] == devolucion)
+                {
+                    Console.WriteLine("devolucion: " + precio[i]);
+                    total = total - precio[i];
+                }
+            }
+            Console.WriteLine("total: " + total);
+        }
+    }
+}
